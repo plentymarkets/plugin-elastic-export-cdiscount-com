5ae67bc369da1431f14ec607d70d220ccaa36c66
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2018-05-02 12:11:59 +0200

    Update CdiscountCOM.php

--- src/ResultField/CdiscountCOM.php
+++ src/ResultField/CdiscountCOM.php
@@ -73,7 +73,7 @@ class CdiscountCOM extends ResultFields
         /**
          * @var LanguageMutator $languageMutator
          */
-		$languageMutator = pluginApp(LanguageMutator::class, ['language' => [$settings->get('lang')]]);
+		$languageMutator = pluginApp(LanguageMutator::class, ['languages' => [$settings->get('lang')]]);
 
         /**
          * @var SkuMutator $skuMutator
